1bd8dd13c6a59479a60010607077ce76a0ab4e3a
PARCELLE007 <bayarmustaphainat@gmail.com>
2025-07-19 09:12:50 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'cf-pwa-v1';
+const urlsToCache = [
+  './',
+  './CF.html', // Mets ici le vrai nom de ton fichier HTML !
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+  // Ajoute d'autres fichiers si tu en as (ex: .css, .js, images)
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
